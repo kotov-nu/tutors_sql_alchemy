bad0f403f336e76845c0e475e16404cbd8477de7
kotov-nu <80386.sl@gmail.com>
2020-02-09 01:29:43 +0300

    Рефакторинг. Базовый шаблон. Сохранение результатов в базе. Правка базы.

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from flask import Flask, render_template, request
-from data import goals, teachers
+from data import teachers
 from json import dumps, loads
 from random import shuffle
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField
+
+goals = {"travel": "⛱ Для путешествий", "study": "🏫 Для школы", "work": "🏢 Для работы",
+         "relocate": "🚜 Для переезда"}
 
 with open('teachers.json', 'w') as f:
     teachers_json = dumps(teachers)
     f.write(teachers_json)
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.secret_key = 'come_to_me_or_not'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///_database.db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class Tutor(db.Model):
+    """ Учителя """
 
     __tablename__ = "tutor"
 
@@ -27,11 +36,11 @@ class Tutor(db.Model):
     goals = db.Column(db.String(), nullable=False)
     free = db.Column(db.String(), nullable=False)
 
-    bookings = db.relationship('Booking', back_populates='client')
-    picks = db.relationship('Pick', back_populates='client')
+    bookings = db.relationship('Booking', back_populates='tutor_info')
 
 
 class Booking(db.Model):
+    """  Бронь занятия """
 
     __tablename__ = "booking"
 
@@ -39,30 +48,21 @@ class Booking(db.Model):
     client_name = db.Column(db.String(), nullable=False)
     client_phone = db.Column(db.String(), nullable=False)
 
-    client_id = db.Column(db.Integer, db.ForeignKey("tutor.id"))
-    client = db.relationship("Tutor", back_populates="bookings")
+    tutor_id = db.Column(db.Integer, db.ForeignKey("tutor.id"))
+    tutor_info = db.relationship("Tutor", back_populates="bookings")
 
 
 class Pick(db.Model):
+    """ Заявка на подбор учителя """
 
     __tablename__ = "pick"
 
     id = db.Column(db.Integer, primary_key=True)
     client_name = db.Column(db.String(), nullable=False)
     client_phone = db.Column(db.String(), nullable=False)
+    time = db.Column(db.String(), nullable=False)
+    goal = db.Column(db.String(), nullable=False)
 
-    client_id = db.Column(db.Integer, db.ForeignKey("tutor.id"))
-    client = db.relationship("Tutor", back_populates="picks")
-
-
-# with open('teachers.json', 'r') as f:
-#     teachers_dict = loads(f.read())
-#
-#     teachers_list = []
-#     for i, value in teachers_dict.items():
-#         teachers_list.append(value)
-#
-#     shuffle(teachers_list)
 
 tutors = db.session.query(Tutor).all()
 teachers_list = []
@@ -71,7 +71,9 @@ for tutor in tutors:
 
 shuffle(teachers_list)
 
+
 def time_table(id):
+    """ Формируем расписание для вывода """
 
     time_table = {
         '8:00': [],
@@ -84,8 +86,7 @@ def time_table(id):
         '22:00': [],
 
     }
-
-    free = teachers_list[id]['free']
+    free = loads(teachers_list[id].free.replace('\'', '"').replace('False', 'false').replace('True', 'true'))
     for value in free.values():
         time_table['8:00'].append(value['8:00'])
         time_table['10:00'].append(value['10:00'])
@@ -100,6 +101,7 @@ def time_table(id):
 
 
 def data_transfer():
+    """ Перенос данных из файла в БД """
 
     with open('teachers.json', 'r') as f:
         teachers_dict = loads(f.read())
@@ -109,8 +111,11 @@ def data_transfer():
             teachers_list.append(value)
 
         for teacher in teachers_list:
-            t = Tutor(name=teacher['name'], about=teacher['about'], rating=teacher['rating'],
-                      picture=teacher['picture'], price=teacher['price'], goals=str(teacher['goals']), free=str(teacher['free']))
+            t = Tutor(
+                name=teacher['name'], about=teacher['about'], rating=teacher['rating'],
+                picture=teacher['picture'], price=teacher['price'], goals=str(teacher['goals']),
+                free=str(teacher['free']
+                         ))
 
             db.session.add(t)
 
@@ -119,33 +124,37 @@ def data_transfer():
 
 # data_transfer()
 
+class ClientInfo(FlaskForm):
+    name = StringField('Вас зовут', validators=[DataRequired()])
+    phone = StringField('Телефон', validators=[DataRequired()])
+
+
+class PickInfo(FlaskForm):
+    goal = SelectField('Какая цель занятий?',
+                       choices=[("travel", "Для путешествий"), ("study", "Для школы"),
+                                ("work", "Для работы"), ("relocate", "Для переезда")],
+                       validators=[DataRequired()])
+    time = SelectField('Сколько времени есть?',
+                       choices=[('1-2', '1-2 часа в неделю'), ('3-5', '3-5 часов в неделю'),
+                                ('5-7', '5-7 часов в неделю'), ('7-10', '7-10 часов в неделю')],
+                       validators=[DataRequired()])
+    name = StringField('Вас зовут', validators=[DataRequired()])
+    phone = StringField('Телефон', validators=[DataRequired()])
+
 
 def selection_teachers_by_goal(goal, teachers_list):
+    """ Отбираем учителей по целям """
     suitable_teachers = []
     for teacher in teachers_list:
-        if goal in teacher['goals']:
+        if goal in teacher.goals:
             suitable_teachers.append(teacher)
 
     return suitable_teachers
 
 
-def save_pick_info(id, name, phone):
-
-    with open('booking.json', 'r') as f:
-
-        all_info = f.read()
-        if all_info:
-            loads(all_info).update({id: {'name': name, 'phone': phone}})
-        else:
-            all_info = {}
-            all_info.update({id: {'name': name, 'phone': phone}})
-
-    with open('booking.json', 'w') as f:
-        f.write(dumps(all_info))
-
-
 @app.route('/')
 def index():
+    print(teachers_list)
     return render_template('index.html', teachers=teachers_list[:6], goals=goals)
 
 
@@ -162,27 +171,36 @@ def goal(goal):
 
 @app.route('/booking/<int:id>')
 def booking(id):
-    return render_template('booking.html', teacher=teachers_list[id], id=id)
+    form = ClientInfo()
+    return render_template('booking.html', teacher=teachers_list[id], id=id, form=form)
 
 
-@app.route('/sent/<int:id>', methods=['GET', 'POST'])
+@app.route('/sent/<int:id>', methods=['POST'])
 def sent(id):
 
-    name = request.form.get('name')
-    phone = request.form.get('phone')
-    save_pick_info(id, name, phone)
+    form = ClientInfo()
+    b = Booking(client_id=teachers_list[id].id, client_name=form.name.data, client_phone=form.phone.data)
+    db.session.add(b)
+    db.session.commit()
 
-    return render_template('sent.html', teacher=teachers_list[id], name=name, phone=phone)
+    return render_template('sent.html', teacher=teachers_list[id], name=form.name.data, phone=form.phone.data)
 
 
-# @app.route('/sent/<int:id>', methods=['GET', 'POST'])
-# def sent(id):
-#
-#     name = request.form.get('name')
-#     phone = request.form.get('phone')
-#
-#     return render_template('sent.html', teacher=teachers_list[id], name=name, phone=phone)
+@app.route('/pick/')
+def pick():
+    form = PickInfo()
+    return render_template('pick.html', form=form)
 
+
+@app.route('/sent_pick/', methods=['POST'])
+def sent_pick():
+
+    form = PickInfo()
+    p = Pick(goal=form.goal.data, time=form.time.data, client_name=form.name.data, client_phone=form.phone.data)
+    db.session.add(p)
+    db.session.commit()
+
+    return render_template('sent_pick.html', name=form.name.data, phone=form.phone.data)
 
 
 app.run('0.0.0.0', 8000)
